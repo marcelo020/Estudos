1d1ab577da59e633e71bb96c1b9f97726146844a
marcelo020 <64228945+marcelo020@users.noreply.github.com>
2026-04-07 15:57:56 -0300

    Create estrutura.java

---
+++ Exercicios/estrutura.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class estrutura {
+    public static void main(String[] args) {
+    int n, s=0;
+    String resp;
+    Scanner teclado = new Scanner(System.in);
+    do {
+        System.out.print("Digite um numero: ");
+    n = teclado.nextInt();
+    s += n;
+    System.out.print("Deseja continuar? [S/N]");
+    resp = teclado.next();
+    } while (resp.equals("S"));
+
+
+    }
+}
